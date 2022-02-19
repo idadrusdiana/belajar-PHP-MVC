a063ade5604554107b190202174e03952281c0e9
idad rusdiana <idadrusdiana01@gmail.com>
2022-02-19 17:28:16 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use ProgrammerZamanNow\Belajar\PHP\MVC\App\Router;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\HomeController;
+
+Router::add('GET', '/', HomeController::class, 'index');
+Router::add('GET', '/hello', HomeController::class, 'hello');
+Router::add('GET', '/world', HomeController::class, 'world');
+
+Router::run();
